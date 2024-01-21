f1742f1058689d99b4408cb74fbc0e060dcf18d7
Venessa N. Maduka <75411816+vennyv@users.noreply.github.com>
2024-01-21 17:01:11 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+var name_typed = new Typed(".auto-type-name", {
+    strings: ["My name is <span class='highlight-name'>Venessa Maduka.</span>"],
+    typespeed: 32,
+    backSpeed: 30,
+    loop: false,
+    showCursor: false
+})
+
+var bio_typed = new Typed(".auto-type-bio", {
+    strings: ["an aspiring full-stack developer.", "a student at Boston University College of Engineering.", "a lover of all things tech!"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true
+})
